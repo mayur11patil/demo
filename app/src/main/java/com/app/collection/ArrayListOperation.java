3fa4a84e87532c674d03f0117c5bd72b0e60f010
mayur.patil <mayur.patil@ATMECSINLT-135.ATMECS.CORP>
2019-06-20 18:09:34 +0530

    code corrected

--- app/src/main/java/com/app/collection/ArrayListOperation.java
+++ app/src/main/java/com/app/collection/ArrayListOperation.java
@@ -7,25 +7,24 @@ import java.util.Scanner;
 
 public class ArrayListOperation {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws CustomException {
 		
 		Scanner sc = new Scanner(System.in);
-		System.out.println("enter no");
 		int ch;
-		String firstName;
-		String lastName;
-		int empId;
+		String firstName=null;
+		String lastName=null;
+		int empId=0;
 		
 		Employee emp;
 		List<Employee> empList = new ArrayList<Employee>();
 		
-	
 		do {
 			
 			System.out.println("1.Add employee record");
 			System.out.println("2.Remove employee record");
 			System.out.println("3.Display All employee record");
 			System.out.println("4.Search for employee record");
+			System.out.println("5.Update employee record");
 			System.out.println("0.Exit");
 			
 			System.out.println("enter which operation you want to perform");
@@ -34,12 +33,20 @@ public class ArrayListOperation {
 			switch (ch) {
 			case 1:
 			{
+				try {
 				System.out.println("enter first name of employee");
 				firstName = sc.next();
+				UserExceptions.validateFirstAndLastName(firstName);
 				System.out.println("enter last name of employee");
 				lastName =sc.next();
+				UserExceptions.validateFirstAndLastName(lastName);
 				System.out.println("enter id for employee");
 				empId = sc.nextInt();
+				}
+				catch (Exception e) {
+					System.out.println(e);
+					break;
+				}
 				
 				emp = new Employee(firstName, lastName, empId);
 				empList.add(emp);
@@ -47,6 +54,7 @@ public class ArrayListOperation {
 			}
 			case 2:
 			{
+				boolean found=true;
 				System.out.println("enter empId to remove the record");
 				empId = sc.nextInt();
 				
@@ -56,11 +64,12 @@ public class ArrayListOperation {
 					if(itr.next().getEmpId()==empId) {
 						itr.remove();
 						System.out.println("Employee record successfully deleted....");
-					}
-					else {
-						System.out.println("EMployee record does not found... enter correct empId");
+						found=true;
 					}
 				}
+				if(!found) {
+					System.out.println("EMployee record does not found... enter correct empId");
+			}
 				
 				/*try {
 				for(Employee empobj:empList) {
@@ -84,21 +93,24 @@ public class ArrayListOperation {
 			
 			case 4:
 			{
+				boolean found=false;
 				System.out.println("enter employee empId to search");
 				empId=sc.nextInt();
 				
 				for(Employee empobj:empList) {
 					if(empobj.getEmpId()==empId) {
 						System.out.println(empobj);
+						found = true;
 					}
-					else {
-						System.out.println("EMployee record does not found... enter correct empId");
-					}
-				break;
 				}
+				if(!found) {
+						System.out.println("EMployee record does not found... enter correct empId");
+				}
+				break;
 			}
 			case 5:
 			{
+				boolean found=false;
 				System.out.println("enter employee empId to update");
 				empId=sc.nextInt();
 				
@@ -112,11 +124,12 @@ public class ArrayListOperation {
 						empobj.setFirstName(firstName);
 						empobj.setLastName(lastName);
 						System.out.println("employee record updated");
-					}
-					else {
-						System.out.println("EMployee record does not found... enter correct empId");
+						found=true;
 					}
 				}
+				if(!found) {
+					System.out.println("EMployee record does not found... enter correct empId");
+			}
 				System.out.println(empList);
 				break;
 			}
